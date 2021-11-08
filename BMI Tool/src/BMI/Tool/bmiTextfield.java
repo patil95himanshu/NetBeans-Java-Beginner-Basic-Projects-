ab71a668327486ed6083c98f47e1cd06a803dd71
HIMANSHU PATIL <74021207+patil95himanshu@users.noreply.github.com>
2021-11-09 03:25:26 +0530

    BMI Calculator.

---
+++ BMI Tool/src/BMI/Tool/bmiTextfield.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package BMI.Tool;
+
+/**
+ *
+ * @author admin
+ */
+class bmiTextfield {
+
+    static void setText(String format) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
